5d38bea6fad71e666882bf0b3229396d948339a9
novichkovv <novichkovv@bk.ru>
2022-06-14 23:15:45 +0800

    initial commit

---
+++ src/LaravelApiHandler.php
@@ -0,0 +1,18 @@
+<?php
+namespace Novichkov\LaravelApiHandler;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Throwable;
+
+class LaravelApiHandler extends ExceptionHandler
+{
+    public function render($request, Throwable $e): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        return response()->json([
+            'status' => 'fail',
+            'error' => 'Laravel Api Handler'
+        ]);
+    }
+}
