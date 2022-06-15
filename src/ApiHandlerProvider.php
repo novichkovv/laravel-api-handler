4790f16b20a686a0a5f1819e1cc4804b8fa2d4f8
novichkovv <novichkovv@bk.ru>
2022-06-15 15:48:55 +0800

    Configuration Path

--- src/ApiHandlerProvider.php
+++ src/ApiHandlerProvider.php
@@ -12,10 +12,10 @@ class ApiHandlerProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/api_handler.php' => config_path('api_handler.php'),
+            __DIR__.'/config/api_handler.php' => config_path('api_handler.php'),
         ]);
         $this->mergeConfigFrom(
-            __DIR__.'/../config/api_handler.php', 'api_handler'
+            __DIR__.'/config/api_handler.php', 'api_handler'
         );
         if(config('api_handler.handle.auto_discover') === true) {
             $this->app->bind(ExceptionHandler::class, ApiHandler::class);
