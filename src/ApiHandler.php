a06ff9fdd72d31d7e6a126d97da5630b4e1e2f4e
novichkovv <novichkovv@bk.ru>
2022-06-15 11:21:32 +0800

    Package Name

--- src/ApiHandler.php
+++ src/ApiHandler.php
@@ -1,12 +1,12 @@
 <?php
-namespace Novichkov\LaravelApiHandler;
+namespace Novichkov\ApiHandler;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Throwable;
 
-class LaravelApiHandler extends ExceptionHandler
+class ApiHandler extends ExceptionHandler
 {
     public function render($request, Throwable $e): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response
     {
